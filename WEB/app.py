d9bbe18cbdaa1548a1e94d2b9095474317d757b6
barsalo221 <barsalo03@gmail.com>
2026-05-24 18:06:56 +0300

    TEST FOR argo-31

--- WEB/app.py
+++ WEB/app.py
@@ -38,4 +38,4 @@ def add_car():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
